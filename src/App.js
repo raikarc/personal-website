38bff554ded49a3c20db967bb2b3d821511f561d
Raika Roy Choudhury <raikarc27@gmail.com>
2024-03-28 22:27:42 -0700

    Changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Raika is a dumbass <code>src/App.js</code> and save to reload.
+          Raika is a sweetiepie <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
